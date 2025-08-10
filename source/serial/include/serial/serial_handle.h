6ee48c521484d4377f14b0f8bcabebf3ab6935b3
mahesh <maheshcodes12@gmail.com>
2025-08-10 21:40:41 +0530

    implemented serialization

---
+++ source/serial/include/serial/serial_handle.h
@@ -0,0 +1,20 @@
+#ifndef SERIAL_HANDLE_H
+#define SERIAL_HANDLE_H 1
+
+/* -- Headers -- */
+
+#include <serial/serial_api.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* -- Type Definitions -- */
+
+typedef void *serial_handle;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SERIAL_HANDLE_H */
